899cda8e3d8536976679eba5d06776dcb25043bf
Monir Hossain <monirair@yahoo.com>
2021-05-10 10:59:18 +0600

    add buyHome data

--- data/buyHome.js
+++ data/buyHome.js
@@ -1,7 +1,13 @@
 const buyHome = [
   {
     _id: '1',
-    name: 'Name of',
+    name: 'Mission Glen, Houston, TX 77083',
+    street: '15903 Williwaw Dr',
+    description:
+      'A beautiful two-story, brick veneer home on a corner lot across from the park, featuring, open concept floor plan, tile flooring across the common areas and wood on the second floor. Spacious bedrooms, Game room, formal dining, zoned to the wonderful Fort Bend ISD. Easy access to West Park Tollway, restaurants and shopping.',
+    size: '1505 sqr/feet',
+    city: 'Houston',
+    seller: 'Jhon Doe',
   },
 ];
 export default buyHome;
