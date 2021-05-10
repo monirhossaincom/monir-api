b165f16e45a6c452a2300f0dc81dd095dcb604ca
Monir Hossain <monirair@yahoo.com>
2021-05-10 20:51:38 +0600

    Add data

--- data/buyHome.js
+++ data/buyHome.js
@@ -6,11 +6,76 @@ const buyHome = [
     info:
       'A beautiful two-story, brick veneer home on a corner lot across from the park, featuring, open concept floor plan, tile flooring across the common areas and wood on the second floor. Spacious bedrooms, Game room, formal dining, zoned to the wonderful Fort Bend ISD. Easy access to West Park Tollway, restaurants and shopping.',
     image:
-      'https://res.cloudinary.com/monirair/image/upload/v1620652011/buy-home/4_yvy3tt.jpg',
-    sellerName: 'Jhon Doe',
+      'https://res.cloudinary.com/monirair/image/upload/v1620652011/buy-home/3_mvz16q.jpg',
+    sellerName: 'Jon Doe',
     sellerImage:
       'https://res.cloudinary.com/monirair/image/upload/v1620653823/buy-home/team4_htkj1b.jpg',
-    price: '1,995',
+    price: '275,000',
+  },
+  {
+    id: '2',
+    name: 'Mission Glen, Houston, TX 77083',
+    street: '15903 Williwaw Dr',
+    info:
+      'A beautiful two-story, brick veneer home on a corner lot across from the park, featuring, open concept floor plan, tile flooring across the common areas and wood on the second floor. Spacious bedrooms, Game room, formal dining, zoned to the wonderful Fort Bend ISD. Easy access to West Park Tollway, restaurants and shopping.',
+    image:
+      'https://res.cloudinary.com/monirair/image/upload/v1620652011/buy-home/2_ceqdw2.jpg',
+    sellerName: 'Jon Doe',
+    sellerImage:
+      'https://res.cloudinary.com/monirair/image/upload/v1620653823/buy-home/team4_htkj1b.jpg',
+    price: '195,000',
+  },
+  {
+    id: '3',
+    name: 'Mission Glen, Houston, TX 77083',
+    street: '15903 Williwaw Dr',
+    info:
+      'A beautiful two-story, brick veneer home on a corner lot across from the park, featuring, open concept floor plan, tile flooring across the common areas and wood on the second floor. Spacious bedrooms, Game room, formal dining, zoned to the wonderful Fort Bend ISD. Easy access to West Park Tollway, restaurants and shopping.',
+    image:
+      'https://res.cloudinary.com/monirair/image/upload/v1620652011/buy-home/5_ekw4jm.jpg',
+    sellerName: 'Jon Doe',
+    sellerImage:
+      'https://res.cloudinary.com/monirair/image/upload/v1620653823/buy-home/team4_htkj1b.jpg',
+    price: '235,000',
+  },
+  {
+    id: '4',
+    name: 'Mission Glen, Houston, TX 77083',
+    street: '15903 Williwaw Dr',
+    info:
+      'A beautiful two-story, brick veneer home on a corner lot across from the park, featuring, open concept floor plan, tile flooring across the common areas and wood on the second floor. Spacious bedrooms, Game room, formal dining, zoned to the wonderful Fort Bend ISD. Easy access to West Park Tollway, restaurants and shopping.',
+    image:
+      'https://res.cloudinary.com/monirair/image/upload/v1620652011/buy-home/1_tbodtt.jpg',
+    sellerName: 'Jon Doe',
+    sellerImage:
+      'https://res.cloudinary.com/monirair/image/upload/v1620653823/buy-home/team4_htkj1b.jpg',
+    price: '255,000',
+  },
+  {
+    id: '5',
+    name: 'Mission Glen, Houston, TX 77083',
+    street: '15903 Williwaw Dr',
+    info:
+      'A beautiful two-story, brick veneer home on a corner lot across from the park, featuring, open concept floor plan, tile flooring across the common areas and wood on the second floor. Spacious bedrooms, Game room, formal dining, zoned to the wonderful Fort Bend ISD. Easy access to West Park Tollway, restaurants and shopping.',
+    image:
+      'https://res.cloudinary.com/monirair/image/upload/v1620652011/buy-home/4_yvy3tt.jpg',
+    sellerName: 'Jon Doe',
+    sellerImage:
+      'https://res.cloudinary.com/monirair/image/upload/v1620653823/buy-home/team4_htkj1b.jpg',
+    price: '159,000',
+  },
+  {
+    id: '6',
+    name: 'Mission Glen, Houston, TX 77083',
+    street: '15903 Williwaw Dr',
+    info:
+      'A beautiful two-story, brick veneer home on a corner lot across from the park, featuring, open concept floor plan, tile flooring across the common areas and wood on the second floor. Spacious bedrooms, Game room, formal dining, zoned to the wonderful Fort Bend ISD. Easy access to West Park Tollway, restaurants and shopping.',
+    image:
+      'https://res.cloudinary.com/monirair/image/upload/v1620652011/buy-home/4_yvy3tt.jpg',
+    sellerName: 'Jon Doe',
+    sellerImage:
+      'https://res.cloudinary.com/monirair/image/upload/v1620653823/buy-home/team4_htkj1b.jpg',
+    price: '285,000',
   },
 ];
 
