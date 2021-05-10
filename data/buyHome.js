08003840bcfb8b14e9497580bd593cd887cbd3c1
Monir Hossain <monirair@yahoo.com>
2021-05-10 23:32:41 +0600

    update the buy home data

--- data/buyHome.js
+++ data/buyHome.js
@@ -2,61 +2,52 @@ const buyHome = [
   {
     id: '1',
     name: 'Mission Glen, Houston, TX 77083',
-    street: '15903 Williwaw Dr',
     info:
       'A beautiful two-story, brick veneer home on a corner lot across from the park, featuring, open concept floor plan, tile flooring across the common areas and wood on the second floor. Spacious bedrooms, Game room, formal dining, zoned to the wonderful Fort Bend ISD. Easy access to West Park Tollway, restaurants and shopping.',
     image:
       'https://res.cloudinary.com/monirair/image/upload/v1620652011/buy-home/3_mvz16q.jpg',
-    sellerName: 'Jon Doe',
+    sellerName: 'Glenn Maxwell',
     sellerImage:
-      'https://res.cloudinary.com/monirair/image/upload/v1620653823/buy-home/team4_htkj1b.jpg',
+      'https://res.cloudinary.com/monirair/image/upload/v1620653823/buy-home/team5_l5vj58.jpg',
     price: '275,000',
   },
   {
     id: '2',
-    name: 'Mission Glen, Houston, TX 77083',
-    street: '15903 Williwaw Dr',
-    info:
-      'A beautiful two-story, brick veneer home on a corner lot across from the park, featuring, open concept floor plan, tile flooring across the common areas and wood on the second floor. Spacious bedrooms, Game room, formal dining, zoned to the wonderful Fort Bend ISD. Easy access to West Park Tollway, restaurants and shopping.',
+    name: 'Lakes of Savannah, Rosharon, TX 77583',
+    info: `Gorgeous home in the charming and sought after subdivision of Lakes of Savannah! This well maintained traditional home features 3 bedrooms 2 bathrooms and PLENTY of space! Designed for effortless entertaining! It is warmed by a cozy fireplace, large office/study & a beautiful master retreat! Lots of cabinet space in the kitchen, extra counter space for cooking! Highlighted by an abundance of natural light! GREAT sized backyard featuring a massive pool, an absolute oasis! Zoned to exemplary schools! Close to major freeways and highways! Must see today!`,
     image:
       'https://res.cloudinary.com/monirair/image/upload/v1620652011/buy-home/2_ceqdw2.jpg',
-    sellerName: 'Jon Doe',
+    sellerName: 'Steve Jobs',
     sellerImage:
-      'https://res.cloudinary.com/monirair/image/upload/v1620653823/buy-home/team4_htkj1b.jpg',
+      'https://res.cloudinary.com/monirair/image/upload/v1620653823/buy-home/team2_x9alqt.jpg',
     price: '195,000',
   },
   {
     id: '3',
-    name: 'Mission Glen, Houston, TX 77083',
-    street: '15903 Williwaw Dr',
-    info:
-      'A beautiful two-story, brick veneer home on a corner lot across from the park, featuring, open concept floor plan, tile flooring across the common areas and wood on the second floor. Spacious bedrooms, Game room, formal dining, zoned to the wonderful Fort Bend ISD. Easy access to West Park Tollway, restaurants and shopping.',
+    name: 'Bellaire West, Houston, TX 77072',
+    info: `Stunning Executive Estate situated on a corner lot tucked away in the exclusive "The Estates" section of Royal Oaks Country Club, this magnificent stone & stucco Mediterranean home carries amazing views of the golf course. This home features 5 bedrooms, 5 full bathrooms and 2 half bathrooms. The home has new interior paint, new hand scraped solid plank hardwood flooring and new carpet. The home also has new light fixtures, plumbing fixtures, door hardware and outdoor gas barbecue grill. The home features 2 wet bars. One off of the den that has a wine room with a new wine chiller for the vintage wine collection and one in the media room for entertaining the family and friends. The kitchen is built for the cook in the family with plenty of storage, 2 sinks, double ovens, 2 dishwashers, new gas cook top and microwave oven. The patio and pool area are great for cooling off in the summer time. The game room is large for entertaining. Come see this home and the many features that it offers.`,
     image:
       'https://res.cloudinary.com/monirair/image/upload/v1620652011/buy-home/5_ekw4jm.jpg',
     sellerName: 'Jon Doe',
     sellerImage:
-      'https://res.cloudinary.com/monirair/image/upload/v1620653823/buy-home/team4_htkj1b.jpg',
+      'https://res.cloudinary.com/monirair/image/upload/v1620653823/buy-home/team3_yzatov.jpg',
     price: '235,000',
   },
   {
     id: '4',
-    name: 'Mission Glen, Houston, TX 77083',
-    street: '15903 Williwaw Dr',
-    info:
-      'A beautiful two-story, brick veneer home on a corner lot across from the park, featuring, open concept floor plan, tile flooring across the common areas and wood on the second floor. Spacious bedrooms, Game room, formal dining, zoned to the wonderful Fort Bend ISD. Easy access to West Park Tollway, restaurants and shopping.',
+    name: 'Fall Creek, Humble, TX 77396',
+    info: `Custom home on the golf course with a pool in Fall Creek. This luxurious custom home has it all, including one of the most gorgeous golf course, lake, and beautiful sunrise views in all of Fall Creek. As you walk into the home, the view of the backyard and golf course from the entry way is breathtaking. The oversized yard has a spectacular pool, hot tub, and overlooks the 16th tee of the Golf Club of Houston. It is a backyard paradise! This exquisite home features a stone and brick elevation, a formal room, and study at the entry, and beautiful wide plank wood floors. The spacious open floor plan has great flow and is perfect for entertaining. Upstairs you will find a game room, media room or flex space, and three large bedrooms. Other key features are the whole-home generator, two 2-car garages, extensive garage storage, reverse osmosis water purifier, gated drive, and fabulous master retreat with a view of the pool and lake.`,
     image:
       'https://res.cloudinary.com/monirair/image/upload/v1620652011/buy-home/1_tbodtt.jpg',
-    sellerName: 'Jon Doe',
+    sellerName: 'Bill Gates',
     sellerImage:
-      'https://res.cloudinary.com/monirair/image/upload/v1620653823/buy-home/team4_htkj1b.jpg',
+      'https://res.cloudinary.com/monirair/image/upload/v1620653823/buy-home/team1_m4uial.jpg',
     price: '255,000',
   },
   {
     id: '5',
-    name: 'Mission Glen, Houston, TX 77083',
-    street: '15903 Williwaw Dr',
-    info:
-      'A beautiful two-story, brick veneer home on a corner lot across from the park, featuring, open concept floor plan, tile flooring across the common areas and wood on the second floor. Spacious bedrooms, Game room, formal dining, zoned to the wonderful Fort Bend ISD. Easy access to West Park Tollway, restaurants and shopping.',
+    name: 'Braes Heights, Houston, TX 77025',
+    info: `The Custom Modern Farmhouse with vintage touches by CastleRock Communities is absolutely stunning! This popular custom Laguna plan is currently under construction, being built upon pier and beam out of the flood zone, and will be ready for a new family before the end of the year. You will fall in love with this dramatic open concept home, it features custom cabinets in the kitchen that go to 12' and open to a 2-story family room with a wall of shiplap, built-ins, balanced out with a modern linear fireplace. Braes Heights prime location is just minutes from the Galleria, Medical Center, parks and conveniently located near major highways! This beautiful home won't last long! Call us today to schedule a private showing!`,
     image:
       'https://res.cloudinary.com/monirair/image/upload/v1620652011/buy-home/4_yvy3tt.jpg',
     sellerName: 'Jon Doe',
@@ -66,13 +57,11 @@ const buyHome = [
   },
   {
     id: '6',
-    name: 'Mission Glen, Houston, TX 77083',
-    street: '15903 Williwaw Dr',
-    info:
-      'A beautiful two-story, brick veneer home on a corner lot across from the park, featuring, open concept floor plan, tile flooring across the common areas and wood on the second floor. Spacious bedrooms, Game room, formal dining, zoned to the wonderful Fort Bend ISD. Easy access to West Park Tollway, restaurants and shopping.',
+    name: 'Southwood, Houston, TX 77004',
+    info: `Gorgeous new construction in the historic and highly sought after Southwood section of Third Ward. This spacious two story, fully brick home features 4 bedrooms, 3.5 bathrooms, formal dining room and upstairs family room. The grand living area feature 20 foot high tray ceilings, fire place and multiple windows allowing natural light to flood the home. The modern yet functional layout of the kitchen is an entertainer's dream as it features an island, granite countertops, high-end stainless steel Samsung appliances and a walk-in pantry. The primary suite is conveniently located on the first floor and provides a retreat to the owner. The primary bathroom features 2 separate walk-in closets, standing shower and separate bathtub. Upstairs, along with the family room, you have the secondary bedrooms and full baths. Backyard features a covered patio with mature landscaping. The homes is within close proximity to Downtown Houston, Texas Medical Center, Museum District and major universities. `,
     image:
       'https://res.cloudinary.com/monirair/image/upload/v1620652011/buy-home/4_yvy3tt.jpg',
-    sellerName: 'Jon Doe',
+    sellerName: 'Stive Kilts',
     sellerImage:
       'https://res.cloudinary.com/monirair/image/upload/v1620653823/buy-home/team4_htkj1b.jpg',
     price: '285,000',
